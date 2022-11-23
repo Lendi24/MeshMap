a4e00587fa6bba4494b7ffb04790e9e7d44a75fa
Lendi24 <Lendic24@hotmail.com>
2022-11-23 12:12:04 +0100

    waffa

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import App from './App';
-import Welcome from './views/Welcome';
+import Welcome from './views/Welcome/Welcome';
 
 import reportWebVitals from './reportWebVitals';
 import {
